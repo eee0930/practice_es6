04d67c5aba7d6871c4a42b0de2b531d0ecbbeee7
lazyhysong <eee0930@naver.com>
2023-06-13 13:37:14 +0900

    feat: method 수정

--- 10.classes/practice_classes.js
+++ 10.classes/practice_classes.js
@@ -44,3 +44,44 @@ const superAdmin = new Admin({
   superAdmin: true,
   isActive: true,
 });
+
+
+
+class Counter {
+  constructor({ initialNumber = 0, counterId, plusId, minusId }) {
+    this.count = initialNumber;
+    this.counter = document.getElementById(counterId);
+    this.counter.innerText = initialNumber;
+    this.plusBtn = document.getElementById(plusId);
+    this.minusBtn = document.getElementById(minusId);
+    this.addEventListeners();
+  }
+  addEventListeners = () => {
+    this.plusBtn.addEventListener("click", this.increase);
+    this.minusBtn.addEventListener("click", this.decrease);
+  };
+  increase = () => {
+    this.count = this.count + 1;
+    this.repaintCounter();
+  }
+  decrease = () => {
+    this.count = this.count - 1;
+    this.repaintCounter();
+  }
+  repaintCounter = () => {
+    this.counter.innerText = this.count;
+  } 
+}
+
+new Counter({
+  counterId: "count",
+  plusId: "plus",
+  minusId: "minus",
+});
+
+new Counter({
+  counterId: "count2",
+  plusId: "plus2",
+  minusId: "minus2",
+  initialNumber: 80,
+});
